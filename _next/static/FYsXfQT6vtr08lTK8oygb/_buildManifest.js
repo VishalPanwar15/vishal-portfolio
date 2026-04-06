55c27f658988e40a40dbfd39ffb522fa0927f1e1
Vishalpanwar15 <vishalpanwarit159@gmail.com>
2026-04-06 09:35:28 +0530

    Updates

--- _next/static/FYsXfQT6vtr08lTK8oygb/_buildManifest.js
+++ _next/static/FYsXfQT6vtr08lTK8oygb/_buildManifest.js
@@ -3,7 +3,7 @@ self.__BUILD_MANIFEST = {
     "afterFiles": [],
     "beforeFiles": [
       {
-        "source": "/.//_next/:path+",
+        "source": "/vishal-portfolio//_next/:path+",
         "destination": "/vishal-portfolio/_next/:path+"
       }
     ],
